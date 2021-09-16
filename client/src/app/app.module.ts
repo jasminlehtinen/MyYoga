060de0580b0417a21534e8dd4b585f2572b2a6ba
Jasmin Lehtinen <jasmin.lehtinen@live.fi>
2021-09-16 18:09:05 +0300

    Added Angular Material for UI-elements
    Main page UI added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,7 +13,10 @@ import { UpdatePoseComponent } from './update-pose/update-pose.component'
 
 import { AppRoutingModule } from './app-routing.module'
 import { PosesService } from './_services/poses.service'
-import { EnvServiceProvider } from './_services/env.service.provider'
+import { EnvServiceProvider } from './_services/env.service.provider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,9 @@ import { EnvServiceProvider } from './_services/env.service.provider'
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [PosesService, EnvServiceProvider],
   bootstrap: [AppComponent]
